3bad7059dfe7f4584d81df6a44fbd441809541dd
petrov-andrey-dev <petrov.a.v-14@yandex.ru>
2024-07-17 23:41:26 +0300

    fix backend url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://127.0.0.1:4000";
+export const URL = "https://api.petrov-andrey-docker.nomorepartiesco.ru/";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
